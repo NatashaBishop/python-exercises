32deb9ecfc28860d7acca0967956fc77a9ff5221
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-05-08 15:31:20 +0100

    Add files via upload

---
+++ python-exercise-4.py
@@ -0,0 +1,6 @@
+# function add_three_numbers takes three parameters and returns their sum.  
+def add_three_numbers(a,b,c):
+    return a+b+c
+#in oder to test the add_three_numbers function I pass 3 numbers to this function and print the result
+result = add_three_numbers(1, 22, 38)
+print("testing add_three_numbers function:", result)
