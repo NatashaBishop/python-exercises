e1baa115b777761b0ee9ea1448db32288235c419
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-05-09 11:16:21 +0100

    Add files via upload

---
+++ python-exercise-5.py
@@ -0,0 +1,5 @@
+# Use the list comprehension to create a list of squares
+# List comprehension syntax to generate a list of squares:
+squares = [x*x for x in range(1, 11)]
+# Print the list of squares
+print("List of squares:", squares)
