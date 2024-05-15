1188e802be81253657a59a95f2241454b777f1d7
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-05-15 22:31:54 +0100

    Update and rename python-exercise-6.py to printDictionaryPairs.py

--- printDictionaryPairs.py
+++ printDictionaryPairs.py
@@ -7,5 +7,6 @@ dictionary = {'hours': 7, 'days': 2, 'workers': 3, 'weeks': 4}
 for key, value in dictionary.items():
 # Print keys and values
     print(f"{key}: {value}")
-    # f-string  is used to fomat string the way we want: key:value where placeholders key and value are replaced 
-    # with actual values while iterating through pairs
+    '''f-string  is used to fomat string the way we want: key:value where placeholders key and value 
+    are replaced 
+    with actual values while iterating through pairs'''
