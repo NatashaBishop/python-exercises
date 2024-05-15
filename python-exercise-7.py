0b1ff5a9ff6ce82c94cf12444c21d11680ed2e23
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-05-15 22:23:50 +0100

    Add files via upload

---
+++ python-exercise-7.py
@@ -0,0 +1,5 @@
+'''Write a Python program that asks the user for their name and age, 
+then prints a message saying how old they will be in 5 years in the format: 
+"Hello [name], you will be [age+5] years old in 5 years." '''
+
+print("Hello,", input("Enter your name: ")+",", "you will be", int(input("Enter your age: "))+5, "years old in 5 years.")
---
+++ python-exercise-8.py
@@ -0,0 +1,7 @@
+    # Python program that requesrts name (string input) and prints out text concatenated with name (string input)
+
+
+request="Your name "
+name = input(request +"is:")
+print (request +"is "+name)
+
