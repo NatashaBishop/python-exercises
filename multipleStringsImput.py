8fa902189f3608500d0e100efd0a88843479adfa
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-06-06 17:59:34 +0100

    Create multipleStringsImput.py

---
+++ multipleStringsImput.py
@@ -0,0 +1,7 @@
+# How to accept 3 separate strings from one input() call
+# .split will separate input by individual strings
+
+str1, str2, str3 = input("Enter three string").split()
+print('str1 is:', str1)
+print('str2 is:', str2)
+print('str3 is:', str3)
