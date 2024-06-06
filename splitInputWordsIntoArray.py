397e7051decad78aa9983543fbdbeb310fff282c
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-06-06 18:31:01 +0100

    Update splitInputWordsIntoArray.py

--- splitInputWordsIntoArray.py
+++ splitInputWordsIntoArray.py
@@ -7,3 +7,8 @@ print('word1 is:', str[0])
 print('word2 is:', str[1])
 print('word3 is:', str[2])
 print('word3 is:', str[3])
+
+# .split is a built-in function and wil split into array any string: 
+#input = "I am xyz"
+#output = input.split()
+#print(output)
