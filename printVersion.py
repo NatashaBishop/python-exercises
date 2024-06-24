85928e02eb70897b28611e56cce7469a024df4a2
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-06-24 16:50:10 +0100

    Create printVersion.py

---
+++ printVersion.py
@@ -0,0 +1,13 @@
+import sys  # Import the sys module to access system-specific parameters and functions
+
+# Print thetext in Python 
+print("Python version: ")
+
+# Use the sys.version attribute to get the Python version and print it
+print(sys.version)
+
+# Print thetext in Python 
+print("Version info:")
+
+# Use the sys.version_info attribute to get detailed version information and print it
+print(sys.version_info)
