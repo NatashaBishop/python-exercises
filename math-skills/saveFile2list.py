178125190b3e1610066b7c936edd2902bb6b0ee1
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-06-26 15:00:53 +0100

    Update saveFile2list.py

--- math-skills/saveFile2list.py
+++ math-skills/saveFile2list.py
@@ -1,7 +1,12 @@
 # Open the file in read mode:
-with open('file.txt', 'r') as file:
+with open('data.txt', 'r') as file:
 lines = file.readlines()   # Read line by line into a list
 
 # Print the list of lines:
 print(lines) 
+
+# or
+with open('data.txt') as f:
+  myIntegerList = [ int(i) for i in f ] 
+  print(myIntegerList)  
 
