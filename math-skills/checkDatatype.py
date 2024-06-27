fb9e45a4537e94c970fea283895b8a37b21e4d35
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-06-27 11:29:41 +0100

    Create checkDatatype.py

---
+++ math-skills/checkDatatype.py
@@ -0,0 +1,13 @@
+a = 42
+b = 3.14
+c = "Hello, World!"
+d = [1, 2, 3]
+e = (1, 2, 3)
+f = {"name": "John", "age": 30}
+
+print(type(a))  # Output: <class 'int'>
+print(type(b))  # Output: <class 'float'>
+print(type(c))  # Output: <class 'str'>
+print(type(d))  # Output: <class 'list'>
+print(type(e))  # Output: <class 'tuple'>
+print(type(f))  # Output: <class 'dict'>
