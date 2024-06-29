eae93fb2b7ab3e7c7897731af1c385074466fd4c
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-06-29 22:24:35 +0100

    Update listAverage.py

--- math-skills/listAverage.py
+++ math-skills/listAverage.py
@@ -3,11 +3,10 @@
 # For Python 3.4+, use mean() from the new statistics module to calculate the average:
 # v.1
 from statistics import mean
-xs = [15, 18, 2, 36, 12, 78, 5, 6, 9]
-mean(xs) 
+list = [15, 18, 2, 36, 12, 78, 5, 6, 9]
+print(mean(list)) 
 
 # v.2
 list = [11, 13, 12, 15, 17]
 import statistics as s
 s.mean(list)
-
