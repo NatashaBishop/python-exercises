6bda430b7599740ce2d86066bf1f4aa2b26d5bed
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-06-30 00:23:50 +0100

    Update listAverage.py

--- math-skills/listAverage.py
+++ math-skills/listAverage.py
@@ -17,3 +17,19 @@ print("Average of the list =", round(result, 2))
 #  from the task: the resuly values are rounded integers, maybe: 
 print("Average of the list =", int(result)) 
 
+# v.3 
+# Python program to get average of a list 
+# Using mean() 
+
+# importing mean() 
+from statistics import mean 
+
+def Average(lst): 
+    return mean(lst) 
+
+# Driver Code 
+lst = [15, 9, 55, 41, 35, 20, 62, 49] 
+average = Average(lst) 
+
+# Printing average of the list 
+print("Average of the list =", round(average, 2)) 
