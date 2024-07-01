f7cd2976753d7c1c2ad5bb3db4955efd624b49ac
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-07-01 19:43:44 +0100

    Update listMiddle.py

--- math-skills/listMiddle.py
+++ math-skills/listMiddle.py
@@ -1,6 +1,11 @@
 # Import statistics Library
 import statistics 
 # The list can be of any size and the numbers are not guaranteed to be in any particular order
-lst = [15, 9, 55, 41, 35, 20, 62, 49] 
+list = [15, 9, 55, 41, 35, 20, 62, 49] 
 # Calculate middle values
 print(statistics.median(list))
+# v.2 
+import numpy as np
+m = np.median([0, 2, 5, 6, 8, 9, 9])
+print("ans:", m)
+# ans: 6.0
