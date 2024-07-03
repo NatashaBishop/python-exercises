07e20cbf3bc3dd390b77043219400c36eefb272d
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-07-03 14:00:28 +0100

    Update listVariance.py

--- math-skills/listVariance.py
+++ math-skills/listVariance.py
@@ -3,3 +3,21 @@
 # It is the average of each point from the mean. 
 
 
+
+# Python3 code to demonstrate working out Variance of List, using mean
+# using loop + formula 
+ 
+# initialize list 
+test_list = [6, 7, 3, 9, 10, 15] 
+ 
+# printing original list 
+print("The original list is : " + str(test_list)) 
+ 
+# Variance of List 
+# using loop + formula 
+mean = sum(test_list) / len(test_list) 
+res = sum((i - mean) ** 2 for i in test_list) / len(test_list) 
+ 
+# printing result 
+print("The variance of list is : " + str(res)) 
+
