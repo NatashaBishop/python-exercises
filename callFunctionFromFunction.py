abc14500f54175d46a4e62be33d7c86c03c8bc9b
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-07-10 14:25:50 +0100

    Create callFunctionFromFunction.py

---
+++ callFunctionFromFunction.py
@@ -0,0 +1,35 @@
+# Example of a Function Call from Another Function in Python.
+# In this example, the SumOfSquares() function calls the Square() which returns the square of the number. 
+
+
+def Square(X):
+    # computes the Square of the given number 
+    # and return to the caller function
+    return (X * X)
+
+def SumofSquares(Array, n):
+
+    # Initialize variable Sum to 0. It stores the 
+    # Total sum of squares of the array of elements
+    Sum = 0
+    for i in range(n):
+
+        # Square of Array[i] element is stored in SquaredValue
+        SquaredValue = Square(Array[i])
+
+        # Cumulative sum is stored in Sum variable
+        Sum += SquaredValue
+    return Sum
+
+# Driver Function
+Array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+n = len(Array)
+
+# Return value from the function 
+# Sum of Squares is stored in Total
+Total = SumofSquares(Array, n)
+print("Sum of the Square of List of Numbers:", Total)
+
+Output : 
+
+Sum of the Square of List of Numbers: 385 
