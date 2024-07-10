e419c010398028c38833b62573956e30b6f1115b
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-07-10 21:11:02 +0100

    Update and rename storeFunction2Variable to storeFunction2Variable.py

---
+++ storeFunction2Variable.py
@@ -0,0 +1,7 @@
+#this function will be stored
+def Hello_World():
+return "Hello, World!"
+#storing Hello_world() (return string) in the variable "world"
+world = Hello_World()
+# call function through the variable
+print(world)
