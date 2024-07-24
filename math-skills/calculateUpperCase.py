01fbe47982965818bf86eae1755651feb123036d
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-07-24 10:03:03 +0100

    Update calculateUpperCase.py

--- math-skills/calculateUpperCase.py
+++ math-skills/calculateUpperCase.py
@@ -6,7 +6,7 @@ s = input()
 # Initialize Dictionary.
 # This initializes a dictionary d with two keys: "UPPER CASE" and "LOWER CASE", both set to 0.
 d={"UPPER CASE":0, "LOWER CASE":0}
-for c in s:
+for c in s: #terate Over String. this iterates over each character c in the string s: 
     if c.isupper():
         d["UPPER CASE"]+=1
     elif c.islower():
