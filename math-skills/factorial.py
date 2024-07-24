d5d16b998628f6db72edc119c0fc9e826c5c07b0
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-07-24 09:44:30 +0100

    Update factorial.py

--- math-skills/factorial.py
+++ math-skills/factorial.py
@@ -1,6 +1,8 @@
 # compute factorial of a given numbers. Results r printed on a single line, comma-separated. 
 #  input data supplied  is a console input
 
+# For Python 3.4+, use fact(x) from the new statistics module to calculate the facorial:
+from statistics import fact
 def fact(x):
     if x == 0:
         return 1
