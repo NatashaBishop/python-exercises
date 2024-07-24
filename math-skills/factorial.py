7525894854558b182a29cb670452ef0d0ffc17ab
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-07-24 09:41:04 +0100

    Update factorial.py

--- math-skills/factorial.py
+++ math-skills/factorial.py
@@ -1,3 +1,10 @@
 # compute factorial of a given numbers. Results r printed on a single line, comma-separated. 
 #  input data supplied  is a console input
 
+def fact(x):
+    if x == 0:
+        return 1
+    return x * fact(x - 1)
+
+x=int(input())
+print(fact(x))
