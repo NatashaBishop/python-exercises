8dea65354ea261175d152cf25e43b5445d995f26
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-07-24 09:54:01 +0100

    Create calculateUpperCase.py

---
+++ math-skills/calculateUpperCase.py
@@ -0,0 +1,3 @@
+# accepts a sentence and calculate the # calculate number of upper case letters and lower case letters. 
+# 4 example: input is "Hello world!". Then, the output will be: UPPER CASE 1 LOWER CASE 9 
+
