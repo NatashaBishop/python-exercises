b74f41eacc1da640bbec90504a6bd7f39a8717ae
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-07-25 12:44:30 +0100

    Create file.py

---
+++ callFunctionFromFile/file.py
@@ -0,0 +1,19 @@
+# all four functions can be called from another file: 
+# here we create a function addition() that we want to call from another file
+
+def addition(a,b):
+#code for adding
+return a +b
+
+def Hello_World():
+#Hi world!
+return "Hello, World!"
+
+def subtract(a,b):
+if a > b:
+return a - b
+else:
+return b - a
+
+def multiply(a,b):
+return a * b;
