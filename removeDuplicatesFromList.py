149cc6c026d80069c2a4006422066f8a5094da83
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-07-26 22:10:21 +0100

    Update removeDuplicatesFromList.py

--- removeDuplicatesFromList.py
+++ removeDuplicatesFromList.py
@@ -1,3 +1,5 @@
 mylist = ["a", "b", "a", "c", "c"]
+# Create a dictionary, using the List items as keys. This will automatically remove any duplicates because dictionaries cannot have duplicate keys: dict.fromkeys(mylist)
+# Then convert the dictionary back into a list: list(...)
 mylist = list(dict.fromkeys(mylist))
 print(mylist) 
