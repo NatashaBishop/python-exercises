d0c7d9a9ba386d60f534b34ce162fe68f3f15078
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-07-28 18:16:53 +0100

    Update and rename exampls4list.py to examples4list.py

--- examples4list.py
+++ examples4list.py
@@ -9,3 +9,10 @@ print(my_list[0])  # 'Jessa'
 # slicing list elements
 print(my_list[1:5])  # ['Kelly', 20, 35.75]
 
+# modify 2nd element of a list
+my_list[1] = "Emma"
+print(my_list[1])  # 'Emma'
+
+# create list using a list class
+my_list2 = list(["Jessa", "Kelly", 20, 35.75])
+print(my_list2)  # ['Jessa', 'Kelly', 20, 35.75]
