34226dc2bc6c172cd9a4c5acaac36810017753db
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-07-28 18:18:59 +0100

    Create examples4tuple .py

---
+++ examples4tuple .py
@@ -0,0 +1,16 @@
+
+
+# create a tuple
+my_tuple = (11, 24, 56, 88, 78)
+print(my_tuple)  # (11, 24, 56, 88, 78)
+print(type(my_tuple))  # class 'tuple'
+
+# Accessing 3rd element of a tuple
+print(my_tuple[2])  # 56
+
+# slice a tuple
+print(my_tuple[2:7])  # (56, 88, 78)
+
+# create a tuple using a tuple() class
+my_tuple2 = tuple((10, 20, 30, 40))
+print(my_tuple2)  # (10, 20, 30, 40)
