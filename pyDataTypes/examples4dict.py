6154b0743360073d23d4171471f216634cd56c40
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-07-29 13:00:49 +0100

    Create examples4dict.py

---
+++ pyDataTypes/examples4dict.py
@@ -0,0 +1,26 @@
+# Python dictionaries are unordered collections of unique values stored in (Key-Value) pairs.
+
+# A heterogeneous (i.e., str, list, tuple) elements are allowed for both key and value in a dictionary. But An object can be a key in a dictionary if it is hashable.
+# The dictionary is mutable which means we can modify its items
+# Dictionary is unordered so we can’t perform indexing and slicing 
+
+# create a dictionary
+my_dict = {1: "Smith", 2: "Emma", 3: "Jessa"}
+
+# display dictionary
+print(my_dict)  # {1: 'Smith', 2: 'Emma', 3: 'Jessa'}
+print(type(my_dict))  # class 'dict'
+
+# create a dictionary using a dit class
+my_dict = dict({1: "Smith", 2: "Emma", 3: "Jessa"})
+
+# display dictionary
+print(my_dict)  # {1: 'Smith', 2: 'Emma', 3: 'Jessa'}
+print(type(my_dict))  # class 'dict'
+
+# access value using a key name
+print(my_dict[1])  # Smith
+
+# change the value of a key
+my_dict[1] = "Kelly"
+print(my_dict[1])  # Kelly
