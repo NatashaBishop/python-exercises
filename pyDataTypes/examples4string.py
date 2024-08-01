b28a98a4d1c3077ddca5d42540e6064657ce8be0
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-08-01 20:40:15 +0100

    Create examples4string.py

---
+++ pyDataTypes/examples4string.py
@@ -0,0 +1,11 @@
+# In Python, A string is a sequence of characters enclosed within a single quote or double quote. 
+# These characters could be anything like letters, numbers, or special symbols enclosed within double quotation marks. For example, "PYnative" is a string. 
+
+platform = "PYnative"
+print(type(platform))  # <class 'str'>
+
+# display string
+print(platform)  # 'PYnative'
+
+# accessing 2nd character of a string
+print(platform[1])  # Y
