6be3f1c0631a57f6c3fae93445106a456caa2c1d
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-08-01 20:42:27 +0100

    Update examples4string.py

--- pyDataTypes/examples4string.py
+++ pyDataTypes/examples4string.py
@@ -8,4 +8,13 @@ print(type(platform))  # <class 'str'>
 print(platform)  # 'PYnative'
 
 # accessing 2nd character of a string
-print(platform[1])  # Y
+print(platform[1])  # Y 
+
+
+# The string is immutable, i.e., it can not be changed once defined. 
+# You need to create a copy of it if you want to modify it. This non-changeable behavior is called immutability. 
+
+platform = "PYnative"
+# Now let's try to change 2nd character of a string.
+platform[0] = 'p'
+# Gives TypeError: 'str' object does not support item assignment
