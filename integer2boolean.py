7fff209a47bbe418f386b56e3b553289a4676896
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-08-11 11:07:17 +0100

    Create integer2boolean.py

---
+++ integer2boolean.py
@@ -0,0 +1,15 @@
+num1 = 10
+num2 = 0
+print(type(num1))  # class 'int'
+
+# Convert into to bool
+b1 = bool(num1)
+b2 = bool(num2)
+
+print(b1)
+# Output True
+print(b2)
+# Output False
+
+print(type(b1))
+# class 'bool'
