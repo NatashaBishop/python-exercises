4e5f58ad57c47562acf1584ef9c52505fafde4f6
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-08-11 11:08:23 +0100

    Create string2boolean.py

---
+++ string2boolean.py
@@ -0,0 +1,17 @@
+s1 = "False"
+s2 = "True"
+s3 = "812"
+s4 = ""
+print(type(s1))  # class 'str'
+
+# Convert string into to bool
+b1 = bool(s1)
+b2 = bool(s2)
+b3 = bool(s3)
+b4 = bool(s4)
+
+print(b1)  # True
+print(b2)  # True
+print(b3)  # True
+print(b4)  # False
+print(type(b1))  # class 'bool'
