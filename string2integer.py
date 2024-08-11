8cdf619c3a4c48250d545499bd24b7c237d632c2
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-08-11 11:17:03 +0100

    Create string2integer.py

---
+++ string2integer.py
@@ -0,0 +1,11 @@
+string_num = "225"
+print(type(string_num))
+# Output class 'str'
+
+# converting str to integer
+num1 = int(string_num)
+
+print("Integer number 1:", num1)
+# Output 225
+print(type(num1))
+# Output class 'int'
