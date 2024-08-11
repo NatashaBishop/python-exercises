5f99798736c52f2380f4978697e44ab998c41a00
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-08-11 11:09:57 +0100

    Create boolean2integer.py

---
+++ boolean2integer.py
@@ -0,0 +1,18 @@
+flag_true = True
+flag_false = False
+print(type(flag_true))
+# Output class 'bool'
+
+# converting boolean to integer
+num1 = int(flag_true)
+num2 = int(flag_false)
+
+print("Integer number 1:", num1)  
+# Output 1
+print(type(num1))  
+# Output class 'int'
+
+print("Integer number 2:", num2)
+# 0
+print(type(num2))
+# class 'int'
