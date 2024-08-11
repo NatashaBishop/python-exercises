b9058a1605cf3ef456143e366a6a9e0978a81695
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-08-11 11:20:36 +0100

    Update string2integer.py

--- string2integer.py
+++ string2integer.py
@@ -9,3 +9,12 @@ print("Integer number 1:", num1)
 # Output 225
 print(type(num1))
 # Output class 'int'
+
+# When converting string type to int type, a string must contain integral value only and should be base-10. Otherwise we will have ValueError: 
+string_num = 'Score is 25'
+print(type(string_num))
+# Output class 'str'
+
+# ValueError: invalid literal for int() with base 10: 'Score is 25'
+num = int(string_num)
+print(num)
