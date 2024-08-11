d3d1739be2d36d8d71fb61a2997552a84b58af85
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-08-11 11:03:56 +0100

    Create string2float.py

---
+++ string2float.py
@@ -0,0 +1,13 @@
+# inspired by: https://pynative.com/python-type-casting/
+
+string_num = "725.535"
+print(type(string_num))
+# Output class 'str'
+
+# converting str to float
+num1 = float(string_num)
+
+print("Float number:", num1)
+# Output 725.535
+print(type(num1))
+# class 'float'
