c0cb54bac0dc0b1d246d35b1de53715e2e2d0405
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-08-28 10:34:12 +0100

    Create reverseString.py

---
+++ reverseString.py
@@ -0,0 +1,7 @@
+def my_function(x): 
+# take string x, start from the end and go backwards to the start by one position (-1)
+  return x[::-1]
+
+mytxt = my_function("I wonder how this text looks like backwards")
+
+print(mytxt) 
