31a831f8a2dd2246a2ee64857233cef1add86120
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-09-03 22:39:18 +0100

    Update write2existingFile.py

--- write2existingFile.py
+++ write2existingFile.py
@@ -5,3 +5,12 @@ f.close()
 #open and read the file after the appending:
 f = open("demofile2.txt", "r")
 print(f.read()) 
+
+# Open the file "demofile3.txt" and overwrite the content:
+f = open("demofile3.txt", "w")
+f.write("Woops! I have deleted the content!")
+f.close()
+
+#open and read the file after the overwriting:
+f = open("demofile3.txt", "r")
+print(f.read()) 
