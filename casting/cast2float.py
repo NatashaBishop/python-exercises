c8dbfff312948c9d3b17c0d06139c929fb08e57b
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-09-08 22:12:21 +0100

    Update cast2float.py

--- casting/cast2float.py
+++ casting/cast2float.py
@@ -1,3 +1,7 @@
+# any basic data type can be put into float(). 
+# it has 2 b a data convertable into float. 
+# 4 example, float("test") will give an error
+
 x = float(1)
 y = float(2.8)
 z = float("3")
