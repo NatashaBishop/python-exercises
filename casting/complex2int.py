445140536c703978fc946b1f6f4db4378063bcee
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-09-09 10:59:04 +0100

    Update complex2int.py

--- casting/complex2int.py
+++ casting/complex2int.py
@@ -2,9 +2,30 @@
 # complex number has two parts, real and imaginary 
 
 # access the real and imaginary part of a complex data type as float respectively:
-#complex data type
+
+# complex data type
 x = 4 + 6j
 #access the real part of the data 
 print(x.real) # Output 4.0
 #access the imaginary part of the data
 print(x.imag) # Output 6.0
+
+# we will first use the real attribute to change the complex number to a float 
+# and then further use the int() function to convert the float data type to an integer: 
+
+#complex data type
+x = 4 + 6j
+ 
+#the given complex number 
+print("The given complex number: ", x)  # Output: The given complex number:  (4+6j)
+ 
+#access the real part of the data and store it in a new variable 
+r = x.real
+ 
+#change the float to int 
+i = int(r) 
+ 
+#the equivalent integer
+print("The equivalent integer: ", i)
+
+
