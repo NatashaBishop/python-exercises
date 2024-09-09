bf433e103bcace2d36470fc9b8544a5ab85d2652
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-09-09 11:09:37 +0100

    Update complex2int.py

--- casting/complex2int.py
+++ casting/complex2int.py
@@ -28,4 +28,7 @@ i = int(r)
 #the equivalent integer
 print("The equivalent integer: ", i) # Output: The equivalent integer:  4
 
-
+# P.S. we cannot convert a larger data type to a smaller data type since it leads to the loss of data. 
+# changing a complex to integer will result in the loss of the imaginary part of the number. 
+# !!!this kind of type casting is not supported by Python
+# there4 we cannot convert directly complex to integer
