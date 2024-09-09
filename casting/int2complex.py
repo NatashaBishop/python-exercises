5ae88a6c22c506b55750a003f47bbee440e4c64b
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-09-09 11:07:19 +0100

    Update int2complex.py

--- casting/int2complex.py
+++ casting/int2complex.py
@@ -8,3 +8,7 @@ print(x) # Output: 4
 
 # print the value of x after type casting it to a complex data type
 print(complex(x)) # Output: (4+0j)
+
+# P.S. we cannot convert a larger data type to a smaller data type since it leads to the loss of data. 
+# changing a complex to integer will result in the loss of the imaginary part of the number. 
+# !!!this kind of type casting is not supported by Python
