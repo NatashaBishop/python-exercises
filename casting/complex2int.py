b2d7bda5c3bf8fc50686fde02662e2370d0344aa
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-09-09 10:54:18 +0100

    Update complex2int.py

--- casting/complex2int.py
+++ casting/complex2int.py
@@ -1,3 +1,11 @@
 # The real numbers are a subset of Complex numbers and the complex numbers are the superset of real numbers.
-
 # complex number has two parts, real and imaginary 
+
+#complex data type
+x = 4 + 6j
+ 
+#access the real part of the data 
+print(x.real) # Output 4.0
+
+#access the imaginary part of the data
+print(x.imag) # Output 6.0
