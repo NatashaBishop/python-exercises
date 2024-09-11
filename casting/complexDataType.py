25666337f53986bc05a498e7ceccb64d0ff45e9b
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-09-11 11:35:39 +0100

    Update complexDataType.py

--- casting/complexDataType.py
+++ casting/complexDataType.py
@@ -9,3 +9,8 @@ print("The complex number is ", z)
 # Output: The complex number is  (3+5j)
 
 # example2: 
+# We can also declare a Complex Data Type in Python using the string literal of type “a + bj”.
+# Defining Complex Number using the string literal
+complex_num = 2 + 4j
+print("The complex number is ", complex_num)
+# Output: The complex number is  (2+4j)
