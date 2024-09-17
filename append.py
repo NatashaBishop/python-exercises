f495cb7080b7e3911e6a2c11e706bd8a0ad37410
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-09-17 20:12:48 +0100

    Create append.py

---
+++ append.py
@@ -0,0 +1,9 @@
+#Initializing an empty list
+fruits = []
+#Adding elements to the list using append()
+fruits.append(‘apple’)
+fruits.append(‘banana’)
+fruits.append(‘orange’)
+
+print(fruits) 
+# Output: [‘apple’, ‘banana’, ‘orange’]
