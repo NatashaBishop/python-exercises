6722d230a3b72bacf53b0a705171963d58e18b3b
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-09-18 23:05:50 +0100

    Create extend.py

---
+++ extend.py
@@ -0,0 +1,4 @@
+# extend() function is a built-in method that is used to append multiple elements from an iterable 
+# (such as another list, tuple, or any other sequence) to the end of an existing list. 
+# unlike the append() method, which adds only one element at a time, extend() allows you to add several elements in a single operation.
+
