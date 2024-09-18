5b661ab33765ce032672c71f59bd94608b77ee0c
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-09-18 23:09:08 +0100

    Update extend.py

--- extend.py
+++ extend.py
@@ -2,3 +2,12 @@
 # (such as another list, tuple, or any other sequence) to the end of an existing list. 
 # unlike the append() method, which adds only one element at a time, extend() allows you to add several elements in a single operation.
 
+# syntax for the extend() function is as follows: list_name.extend(iterable)
+
+# Initializing an empty list
+fruits = [‘apple’, ‘banana’]
+# Adding elements from another list using extend()
+additional_fruits = [‘orange’, ‘grape’, ‘mango’]
+fruits.extend(additional_fruits)
+print(fruits) 
+# Output: [‘apple’, ‘banana’, ‘orange’, ‘grape’, ‘mango’]
