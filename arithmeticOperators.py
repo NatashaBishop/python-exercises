3ff8c0965e11773b5ff2f1dfa89aa90d29e0d9ae
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-09-21 09:44:24 +0100

    Create arithmeticOperators.py

---
+++ arithmeticOperators.py
@@ -0,0 +1,28 @@
+# The precedence of Arithmetic Operators in Python is as follows:
+#    P – Parentheses
+#    E – Exponentiation
+#    M – Multiplication (Multiplication and division have the same precedence)
+#    D – Division
+#    A – Addition (Addition and subtraction have the same precedence)
+#    S – Subtraction
+
+# example1
+a = 9
+b = 4
+add = a + b
+sub = a - b
+mul = a * b
+mod = a % b
+p = a ** b
+print(add)
+print(sub)
+print(mul)
+print(mod)
+print(p)
+
+#Output:
+#13
+#5
+#36
+#1
+#6561
