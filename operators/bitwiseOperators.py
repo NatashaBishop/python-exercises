425d1a7db34f5d07cd5040c296dc84419770ca56
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-09-22 15:23:28 +0100

    Create bitwiseOperators.py

---
+++ operators/bitwiseOperators.py
@@ -0,0 +1,9 @@
+# https://www.geeksforgeeks.org/python-operators/
+
+# bitwise operations manipulate the binary representations of the numbers
+#    Bitwise NOT (~)
+#    Bitwise right shift (>>)
+#    Bitwise left shift (<<)
+#    Bitwise AND (&)
+#    Bitwise XOR (^)
+#    Bitwise OR (|)
