dc363b0a4e5192e54bfdf5fa6c45708cafec8b16
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-09-23 12:41:34 +0100

    Update identityOperators.py

--- operators/identityOperators.py
+++ operators/identityOperators.py
@@ -4,16 +4,27 @@
 # is not      True if the operands are not identical : Returns False if both object refers to same memory location, else returns True. Syntax: obj1 is not obj2
 num1 = 5
 num2 = 5
-
 lst1 = [1, 2, 3]
 lst2 = [1, 2, 3]
 lst3 = lst1
-
 str1 = "hello world"
 str2 = "hello world"
-
 # using 'is' identity operator on different datatypes
 print(num1 is num2)
 print(lst1 is lst2)
 print(str1 is str2)
 print(str1 is str2)
+
+# Python program to illustrate the use of 'is not' identity operator
+num1 = 5
+num2 = 5
+lst1 = [1, 2, 3]
+lst2 = [1, 2, 3]
+lst3 = lst1
+str1 = "hello world"
+str2 = "hello world"
+# using 'is not' identity operator on different datatypes
+print(num1 is not num2)
+print(lst1 is not lst2)
+print(str1 is not str2)
+print(str1 is not str2)
