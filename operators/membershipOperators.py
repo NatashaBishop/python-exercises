42f7adfc9ceb61edbfcb32612e1514744c656229
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-09-27 19:03:20 +0100

    Update membershipOperators.py

--- operators/membershipOperators.py
+++ operators/membershipOperators.py
@@ -1,2 +1,16 @@
 # in            True if value is found in the sequence
 # not in        True if value is not found in the sequence# used to test whether a value or variable is in a sequence
+
+x = 24
+y = 20
+list = [10, 20, 30, 40, 50]
+
+if (x not in list):
+    print("x is NOT present in given list")
+else:
+    print("x is present in given list")
+
+if (y in list):
+    print("y is present in given list")
+else:
+    print("y is NOT present in given list")
