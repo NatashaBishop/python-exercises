17c1a7b8490bfd82a23709bbd1302e79b01de3ab
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-09-28 08:52:57 +0100

    Create ternaryOperators.py

---
+++ operators/ternaryOperators.py
@@ -0,0 +1,13 @@
+#  Ternary operators also known as conditional expressions
+# Ternary Operator determines if a condition is true or false and then returns appropriate value as the result
+# Syntax: true_value if condition else false_value
+
+# Example Ternary If Else:
+a = 10
+b = 20
+# python ternary operator
+min = "a is minimum" if a < b else "b is minimum"
+print(min)
+#Output:
+#a is minimum
+
