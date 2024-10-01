42a67e0923ac4802a0fbad44f478e721f3385312
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-10-01 20:35:33 +0100

    Update bitwiseOperators.py

--- operators/bitwiseOperators.py
+++ operators/bitwiseOperators.py
@@ -23,5 +23,20 @@ print(a << 2)
 # 2
 # 40
 
+# bitwise left shift moves the bits of a number to the left by a specified number of positions
+# In binary, each bit represents a power of 2 
+# and shifting the bits to the left is like multiplying the number by 2 for every shift.
+# example: shift the number 5 (which is 101 in binary) to the left by 1 position. 
+# the bits move one position to the left and a zero is added to the right.
+# The 101 becomes 1010 (which is 10 in decimal)
+#!!!!!!!!!! 
+# Left Shift by 1: << 1 is the same as multiplying by 2.
+# Left Shift by 2: << 2 is the same as multiplying by 4 (2^2).
+# Left Shift by 3: << 3 is the same as multiplying by 8 (2^3), and so on.
+
+b = 5       # Binary: 101
+b = b << 1  # Left shift by 1 (binary 1010)
+print(b)    # Output: 10
+
 
 
