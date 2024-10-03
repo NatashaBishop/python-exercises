9611f94e6d3e6e20d8e374020b6a6367e3cf4e83
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-10-03 14:30:29 +0100

    Create add2vectors.py

---
+++ add2vectors.py
@@ -0,0 +1,7 @@
+# Python script (code) to add two vectors using a non-vectorized approach (for loop)
+list_A = [5,16,66,0,2,9]
+list_B = [5,7,9,22,14,1]
+result = []
+for i in range(len(list_A)):
+    multiply2lists = list_A[i]+list_B[i]
+    result.append(result)
