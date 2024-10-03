e642b4784cbe8cb9152ab789b556921c88bf286f
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-10-03 15:20:42 +0100

    Update add2vectors.py

--- add2vectors.py
+++ add2vectors.py
@@ -2,8 +2,8 @@
 list_A = [5,16,66,0,2,9]
 list_B = [5,7,9,22,14,1]
 result = []
-for i in range(len(list_A)):
-    multiply2lists = list_A[i]+list_B[i]
+for i in range(len(list_A)): #looping throug the list_A and adding item by item from list_B
+    adding2lists = list_A[i] + list_B[i]
     result.append(result)
 
 # Vectorized Addition of Two Vectors
@@ -13,4 +13,4 @@ list_B = [5,7,9,22,14,1]
 result = []
 np_array_A = np.array(list_A)
 np_array_B = np.array(list_B)
-multiply2vectors = np_array_A+ np_array_B
+adding2vectors = np_array_A + np_array_B
