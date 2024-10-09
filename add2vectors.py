3ada3223a97c74c3860855f724c2ad1b84dd4cea
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-10-09 15:30:28 +0100

    Update add2vectors.py

--- add2vectors.py
+++ add2vectors.py
@@ -7,11 +7,11 @@ for i in range(len(list_A)): #looping throug the list_A and adding item by item 
     adding2lists = list_A[i] + list_B[i]
     result.append(result) # adding 2 result every item we went throug while adding
 
-# Vectorized Addition of Two Vectors
+# Vectorized approach for Addition of Two Vectors
 import numpy as np
 list_A = [5,16,66,0,2,9]
 list_B = [5,7,9,22,14,1]
 result = []
-np_array_A = np.array(list_A)
-np_array_B = np.array(list_B)
-adding2vectors = np_array_A + np_array_B
+np_array_A = np.array(list_A) # converting list  list_A into vector with help of numpy (issigned to np @ import stage)
+np_array_B = np.array(list_B) #c above
+adding2vectors = np_array_A + np_array_B # 2 vectors just add @ index level
