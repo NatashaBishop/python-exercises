f0ee6c468727e06503d96fa63a7a37ef19d30a82
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-10-31 22:15:32 +0000

    Update identityOperators.py

--- operators/identityOperators.py
+++ operators/identityOperators.py
@@ -22,7 +22,7 @@ lst2 = [1, 2, 3]
 lst3 = lst1
 str1 = "hello world"
 str2 = "hello world"
-# using 'is' identity operator on different datatypes
+# using 'is' identity operator on different datatypes. will not convert
 print(num1 is num2)
 print(lst1 is lst2)
 print(str1 is str2)
