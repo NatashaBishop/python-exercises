91e9116d854c1345539767588ff0ecd74a7f75c0
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-03 15:55:18 +0000

    Update splitString.py

--- splitString.py
+++ splitString.py
@@ -2,3 +2,10 @@ a = "Hello, World!"
 b = a.split(",")
 print(b)
 #output: ['Hello', ' World!']
+#===== new file
+#split particular index:
+a = "Hello,c World! aaa"
+b = a.split(" ")
+c = b[0].split(",")
+print(b)
+print(c)
