afb39448bb1b61cff3c7728f08da2c91d014774d
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-25 19:25:14 +0000

    Create installNumpy.py

---
+++ numpyExercises/installNumpy.py
@@ -0,0 +1,5 @@
+#inspired by https://github.com/rougier/numpy-100/blob/master/100_Numpy_exercises_with_hints_with_solutions.md
+# pip install numpy
+import numpy as np #np is just an alias
+print(np.__version__)
+np.show_config()
