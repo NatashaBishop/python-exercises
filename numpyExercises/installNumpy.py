536c23c9f20018ba3e19325e98e38c7fc9778c8f
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-26 14:25:27 +0000

    Update installNumpy.py

--- numpyExercises/installNumpy.py
+++ numpyExercises/installNumpy.py
@@ -1,5 +1,11 @@
 #inspired by https://github.com/rougier/numpy-100/blob/master/100_Numpy_exercises_with_hints_with_solutions.md
 # pip install numpy
+
+# Importing the NumPy library with an alias 'np'
 import numpy as np #np is just an alias
+
+# Printing the version of NumPy installed
 print(np.__version__)
-np.show_config()
+
+# Printing the configuration details of NumPy
+print(np.show_config())
