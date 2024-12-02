e497bf8acc9142884a385b9c3ec86128caa04812
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-12-02 16:41:44 +0000

    Update rangeExamples.py

--- rangeExamples.py
+++ rangeExamples.py
@@ -1,4 +1,10 @@
 #inspired by https://pynative.com/python-range-function/
+'''syntax of the range() function: 
+range(start, stop[, step])
+It takes three arguments. Out of the three, two are optional. The start and step are optional arguments and the stop is the mandatory argument.
+start - optiona  argument 
+step - optiona  argument 
+stop - mandatory argument'''
 
 # Generate numbers between 0 to 6
 for i in range(6):
