6f7f8f7756e3cf9abb39878f18cb7eb665218038
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-12-05 11:35:39 +0000

    Update pyModule.py

--- pyModules/pyModule.py
+++ pyModules/pyModule.py
@@ -1,4 +1,12 @@
 # inspired by https://www.analyticsvidhya.com/blog/2021/07/working-with-modules-in-python-must-known-fundamentals-for-data-scientists
+#VARIABLE we are accling data from in another function by importing this file as module
+person1 = {
+  "name": "Chirag Goyal",
+  "age": 19,
+  "country": "India"
+  "education”: “IIT Jodhpur"  
+}
 
+#function we are accling data from in another function by importing this file as module
 def welcome(name):
   print("Hello, " + name +" to Python")
