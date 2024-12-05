91c839f5f9bc80ea7c19eab94a71aa7013241970
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-12-05 11:22:27 +0000

    Create pyModule.py

---
+++ pyModules/pyModule.py
@@ -0,0 +1,4 @@
+# inspired by https://www.analyticsvidhya.com/blog/2021/07/working-with-modules-in-python-must-known-fundamentals-for-data-scientists
+
+def welcome(name):
+  print("Hello, " + name +" to Python")
