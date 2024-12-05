80864b2c35391f8993b6dc7cb65f0a68bc7b285f
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-12-05 11:37:53 +0000

    Update CALLpyModule.py

--- pyModules/CALLpyModule.py
+++ pyModules/CALLpyModule.py
@@ -1,2 +1,9 @@
-import pyModule
+import pyModule 
+#call data from person1 variable from pyModule.py file
+a = mymodule.person1["age"]
+b = mymodule.person1["education"]
+c = mymodule.person1["country"]
+print(a)
+
 pyModule.welcome("Nataliya")
+
