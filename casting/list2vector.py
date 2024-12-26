9f1464910aab537d591ad8eef45725fd1790d106
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-12-26 11:22:20 +0000

    Update list2vector.py

--- casting/list2vector.py
+++ casting/list2vector.py
@@ -1,12 +1,11 @@
 import numpy as np 
+#Horizontal Vector example
 nyLst = [10,20,30,40,50] 
 nyVector = np.array(nyLst) 
-
 print("Vector created from the list nyLst:") 
 print(vctr) 
-
 #Output:
-
 #VVector created from the list nyLst:
 #[10 20 30 40 50]
 
+#vertical Vector example
