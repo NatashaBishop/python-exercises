3f660fed51dc3bbc83b1248b059d27f68ec0d8ad
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-12-27 20:53:02 +0000

    Create vectorPlusVector.py

---
+++ numpyExercises/vectorPlusVector.py
@@ -0,0 +1,14 @@
+#inspired by https://www.digitalocean.com/community/tutorials/vectors-in-python
+import numpy as np 
+
+list1 = [10,20,30,40,50] 
+list2 = [1,2,3,4,5]
+vector1 = np.array(lst1) 
+vector2= np.array(lst2) 
+
+addVectors = vector1+vector2
+print("Addition of two vectors: ",addVectors)
+'''
+Output:
+Addition of two vectors:  [11 22 33 44 55]
+'''
