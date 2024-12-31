6b06730635114dd6bc42e1c93ef19d794d0b485d
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-12-31 18:55:08 +0000

    Create string2datetimeObject.py

---
+++ casting/string2datetimeObject.py
@@ -0,0 +1,4 @@
+#Convert a String to a datetime Object in Python 
+#inspired by: https://www.digitalocean.com/community/tutorials/python-string-to-datetime-strptime
+
+#datetime and time modules both include a strptime() class method to convert strings to objects.
