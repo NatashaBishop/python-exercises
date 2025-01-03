377674b12e854111d3915a8588013f5794a8cbc5
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2025-01-03 10:01:45 +0000

    Update list2vector.py

--- casting/list2vector.py
+++ casting/list2vector.py
@@ -3,7 +3,7 @@ import numpy as np
 nyLst = [10,20,30,40,50] 
 nyVector = np.array(nyLst) 
 print("Vector created from the list nyLst:") 
-print(vctr) 
+print(nyVector) 
 #Output:
 #VVector created from the list nyLst:
 #[10 20 30 40 50]
