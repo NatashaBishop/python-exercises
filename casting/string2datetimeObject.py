621b2d5874de18ae584058849cf90d73e2428eea
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2025-01-03 13:27:47 +0000

    Update string2datetimeObject.py

--- casting/string2datetimeObject.py
+++ casting/string2datetimeObject.py
@@ -4,7 +4,8 @@
 #datetime and time modules both include a strptime() class method to convert strings to objects.
 # The syntax for the datetime.strptime() method:
 # datetime.strptime(date_string, format)
-# Convert String to datetime.datetime() Object Example:
+
+# 1. Convert String to datetime.datetime() Object Example:
 from datetime import datetime
 datetime_str = '09/19/22 13:55:26'
 datetime_object = datetime.strptime(datetime_str, '%m/%d/%y %H:%M:%S')
