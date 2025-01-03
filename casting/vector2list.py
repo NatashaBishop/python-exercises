4bcbeae4698aec0e3a01550e4dc40878b7d73100
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2025-01-03 10:17:34 +0000

    Update vector2list.py

--- casting/vector2list.py
+++ casting/vector2list.py
@@ -8,6 +8,7 @@ print(f'Vector created from the list myLst (NumPy array):\n{myVector}')
 # Vector created from the list myLst (NumPy array):
 # [10 20 30 40 50]
 
+#converting to list with tolist()
 newList = myVector.tolist()
 print(f'new list: {newList}')
 
