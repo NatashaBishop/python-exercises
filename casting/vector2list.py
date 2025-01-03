22182a3efd67391d09df3a8c37a79399563868c7
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2025-01-03 10:10:46 +0000

    Create vector2list.py

---
+++ casting/vector2list.py
@@ -0,0 +1,14 @@
+import numpy as np 
+#create list
+myLst = [10,20,30,40,50] 
+#create vector (numPy array)
+myVector = np.array(myLst) 
+print(f'Vector created from the list myLst (NumPy array):\n{myVector}')
+# Output:
+# Vector created from the list myLst (NumPy array):
+# [10 20 30 40 50]
+
+newList = myVector.tolist()
+print(f'new list: {newList}')
+
+
