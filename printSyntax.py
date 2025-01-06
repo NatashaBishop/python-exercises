8a718ddcda2b867794446ca2e59c58ff9017f4ac
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2025-01-06 18:23:54 +0000

    Update printSyntax.py

--- printSyntax.py
+++ printSyntax.py
@@ -3,3 +3,6 @@ print(f'My text: {myVariable}')
 #Print two messages, and specify the separator:
 print("Hello", "how are you?", sep="---") 
 #output: Hello---how are you? 
+
+print("Hello", "how are you?")
+# output: Hello how are you?
