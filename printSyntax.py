ce53801923bf9002c9a411b40cb3f72a666255c5
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2025-01-06 18:22:40 +0000

    Update printSyntax.py

--- printSyntax.py
+++ printSyntax.py
@@ -1,2 +1,5 @@
 print("Today's date =", myVariableName)
 print(f'My text: {myVariable}')
+#Print two messages, and specify the separator:
+print("Hello", "how are you?", sep="---") 
+#output: Hello---how are you? 
