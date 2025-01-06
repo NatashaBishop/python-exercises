314517f52a2d8edcf76621e0479b5e253eda7d81
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2025-01-06 17:49:41 +0000

    Update examples4dateTime.py

--- pyDataTypes/examples4dateTime.py
+++ pyDataTypes/examples4dateTime.py
@@ -1,5 +1,11 @@
 import datetime
-# getting current date and time
+# getting current date and time:
 curTime = datetime.datetime.now()
 print(curTime)
 #Output: 2022-12-27 08:26:49.219717
+
+#getting current date:
+#import datetime - done in 1st example
+current_date = datetime.date.today()
+print(current_date)
+#Output: 2022-12-27
