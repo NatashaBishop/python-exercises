d414889220b03e82d46a9723acbf9fbeaeb61530
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-01-21 06:50:21 +0000

    Update printSyntax.py

--- printSyntax.py
+++ printSyntax.py
@@ -29,5 +29,6 @@ print("My name is", name, "and I am", age, "years old.")
 #Using String Formatting:
 #using the % operator to format string, followed by a variable name enclosed in curly braces {}
 print("My name is %s and I am %d years old." % (name, age))
+#works similar to .format
 #output: 
 # My name is Natasha and I am 18years old.
