ec277900e5daf33749dad1db3eae171be14e7ef9
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-01-26 12:47:24 +0000

    Update str2list.py

--- stringManipulation/str2list.py
+++ stringManipulation/str2list.py
@@ -6,14 +6,5 @@ list = []
 # initiate string:
 str = "I am xyz"
 list = str.split()
-print('word1 is:', str[0])
-print('word2 is:', str[1])
-print('word3 is:', str[2])
-print('word3 is:', str[3])
 print(list)
 #output: ['I', 'am', 'xyz']
-
-print('word1 in string is:', str[0])
-print('word2 in string is:', str[1])
-print('word3 in string is:', str[2])
-print('word3 in string is:', str[3])
