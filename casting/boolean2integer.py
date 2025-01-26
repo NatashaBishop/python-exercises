7e329c376c7c75f433f0bd4557e74b223cb65f3b
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-01-26 09:25:47 +0000

    Update boolean2integer.py

--- casting/boolean2integer.py
+++ casting/boolean2integer.py
@@ -1,18 +1,20 @@
+#declaring buleans: 
 flag_true = True
 flag_false = False
+#check on datatype:
 print(type(flag_true))
 # Output class 'bool'
 
-# converting boolean to integer
+# converting boolean to integer:
 num1 = int(flag_true)
 num2 = int(flag_false)
 
 print("Integer number 1:", num1)  
-# Output 1
+# Output: 1
 print(type(num1))  
-# Output class 'int'
+# Output: class 'int'
 
 print("Integer number 2:", num2)
-# 0
+# Output: 0
 print(type(num2))
-# class 'int'
+# Output: class 'int'
