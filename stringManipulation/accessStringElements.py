54f4c2e9f37991115d474a5e1c7df473b8b68b91
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-01-26 12:50:21 +0000

    Create accessStringElements.py

---
+++ stringManipulation/accessStringElements.py
@@ -0,0 +1,8 @@
+# initiate string:
+str = "I am xyz"
+
+print('word1 in string is:', str[0])
+print('word2 in string is:', str[1])
+print('word3 in string is:', str[2])
+print('word3 in string is:', str[3])
+# e.t.c.
