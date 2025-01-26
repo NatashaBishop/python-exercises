856a6013b8370f6f93098bc6289a9a0ca30265c4
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-01-26 09:57:58 +0000

    Update storeFunction2Variable.py

--- storeFunction2Variable.py
+++ storeFunction2Variable.py
@@ -1,6 +1,6 @@
 #this function will be stored
 def Hello_World():
-return "Hello, World!"
+  return "Hello, World!"
 #storing Hello_world() (return string) in the variable "world"
 world = Hello_World()
 # call function through the variable
