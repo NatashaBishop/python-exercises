5f8b3559c1b4429ced346969cfc22461d6bf97ff
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-01-26 12:54:48 +0000

    Create accessListElements.py

---
+++ listManipulation/accessListElements.py
@@ -0,0 +1,13 @@
+# How to split a string with phrase into a list of separate strings: 
+# calling .split fanction will separate initial string (by spaces) into individual strings and write them into list
+
+# declare list:
+list = ['I', 'am', 'xyz']
+
+print(list)
+#output: ['I', 'am', 'xyz']
+
+print('index 0 is:', list[0])
+print('index 1 is:', list[1])
+print('index 3 is:', list[2])
+#output: 
