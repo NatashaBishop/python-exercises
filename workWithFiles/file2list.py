c40df890dff70b91ed6250a89fe0686b1718caac
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-02-12 11:34:52 +0000

    Update file2list.py

--- workWithFiles/file2list.py
+++ workWithFiles/file2list.py
@@ -6,3 +6,8 @@ print(my_list)
 with open('input.txt') as f:
   my_list = [x.rstrip() for x in f]
 print(my_list)
+
+#simple way to read lines into list with \n stripped:
+f = open('input.txt') # Open file on read mode
+lines = f.read().splitlines() # List with stripped line-breaks
+f.close() # Close file
