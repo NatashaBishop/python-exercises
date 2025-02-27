270931be74494014becca1bc6044c7535be544d3
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-02-27 15:07:51 +0000

    Update arraySlicing.py

--- numpyExercises/arraySlicing.py
+++ numpyExercises/arraySlicing.py
@@ -6,6 +6,12 @@ print(arr[:2]) #from index [0] to up2 [2], but not including [2]
 
 #Use the minus operator to refer to an index from the end:
 #Slice from the index 3 from the end to index 1 from the end:
-arr = np.array([1, 2, 3, 4, 5, 6, 7])
 print(arr[-3:-1]) 
 #output: [5 6] 
+
+#step value determines the step of the slicing:
+print(arr[1:5:2]) 
+#output: [2 4]
+ 
+
+
