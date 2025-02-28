e9a7d616eb595168f78425de2b627547e16b35da
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-02-28 20:32:36 +0000

    Update vector2list.py

--- casting/vector2list.py
+++ casting/vector2list.py
@@ -12,4 +12,17 @@ print(f'Vector created from the list myLst (NumPy array):\n{myVector}')
 newList = myVector.tolist()
 print(f'new list: {newList}')
 
+#multi-dimensional NumPy Array 2 List:
+# 2d array to list
+arr_2 = np.array([[1, 2, 3], [4, 5, 6]])
+
+print(f'NumPy Array:\n{arr_2}')
+
+# 2d array to list
+arr_2 = np.array([[1, 2, 3], [4, 5, 6]])
+print(f'NumPy Array:\n{arr_2}')
+list_2 = arr_2.tolist()
+print(f'List: {list_2}')
+
+#output: [[1, 2, 3], [4, 5, 6]]
 
