c92bdb6679dda80f1d2d0dd16449297ecf309aa4
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-02-28 20:33:35 +0000

    Update vector2list.py

--- casting/vector2list.py
+++ casting/vector2list.py
@@ -21,6 +21,9 @@ print(f'NumPy Array:\n{arr_2}')
 # 2d array to list
 arr_2 = np.array([[1, 2, 3], [4, 5, 6]])
 print(f'NumPy Array:\n{arr_2}')
+'''NumPy Array output:
+[[1 2 3]
+ [4 5 6]]'''
 list_2 = arr_2.tolist()
 print(f'List: {list_2}')
 
