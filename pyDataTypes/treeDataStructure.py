446997679de5526104a580fb1ac8552938c65d99
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-03-14 18:51:41 +0000

    Create treeDataStructure.py

---
+++ pyDataTypes/treeDataStructure.py
@@ -0,0 +1,2 @@
+# Tree data structure is a hierarchical structure that is used to represent and organize data in the form of parent child relationship
+# In computer science, a tree is a non-linear data structure consisting of nodes connected by edges. It comprises a root node, which serves as the starting point, and each node can have zero or more child nodes.
