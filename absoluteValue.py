31ca4cc0a5979d2c1b9cd9f03de25edfbcff5a66
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-03-16 15:45:36 +0000

    Update absoluteValue.py

--- absoluteValue.py
+++ absoluteValue.py
@@ -2,9 +2,12 @@ x = -2
 absolutValueOFx = abs(x) #prints: 2
 #calculate absolute value of elements in the list
 y = [22,-4, -42] 
-l = [-2, -1, 0, 1, 2]
 
-print([abs(i) for i in l]) #loop through list
-#output: [2, 1, 0, 1, 2]
+print([abs(i) for i in y]) #loop through list
+#output: [22, 4, 42]
 
+import numpy as np
+y = np.array(y)
+print(np.abs(y))
+#output: [22, 4, 42]
 
