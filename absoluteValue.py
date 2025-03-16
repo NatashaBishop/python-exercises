a66f9c434adf52fd236c086a2ecb90a6511db236
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-03-16 15:59:25 +0000

    Update absoluteValue.py

--- absoluteValue.py
+++ absoluteValue.py
@@ -11,7 +11,8 @@ z = np.array(y)
 print(np.abs(z))
 #output: [22, 4, 42]
 
-
-# Using map() to apply abs() to each element
+# Using map() to apply abs() to each element of map and converting it back 2 list
 a = list(map(abs, y))
 print(a)
+#output: [22, 4, 42]
+
