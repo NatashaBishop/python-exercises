3dd8d1e6d101403cadb2094bea14c33ebc10253e
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-03-22 22:04:28 +0000

    Create doubleColon.py

---
+++ listManipulation/doubleColon.py
@@ -0,0 +1,8 @@
+# syntax: collection[start:stop:step]  
+# collection = list, string, array, and so on
+# start = where the slicing  tarts
+# stop = where the operation stops
+# step = the sequence of iterating through the elements. 2 means every second element, 3 - every third
+list1 = [2,4,6,8,10,12]
+print(list1[2:])
+# [6, 8, 10, 12]
