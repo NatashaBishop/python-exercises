62634a55f671f87bd055483f27cfbce38bbd4e27
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-03-29 21:12:58 +0000

    Update np.array.py

--- numpyExercises/np.array.py
+++ numpyExercises/np.array.py
@@ -32,3 +32,7 @@ np.array(np.asmatrix('1 2; 3 4'))
 np.array(np.asmatrix('1 2; 3 4'), subok=True)
 #output: matrix([[1, 2],
 #output:         [3, 4]])
+
+'''Multidimensional lists in Python can have inconsistent types or dimensions, making conversion to a NumPy array impossible. 
+For example, the list''' 
+a = [[1, 2, 3, 4], [5, 6, 7], [8, 9]] #cannot be converted into a NumPy array.
