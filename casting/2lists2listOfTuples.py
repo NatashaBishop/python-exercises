22f2e031d1e90bfead7ae1b7cff4556b33e83efd
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-04-28 01:45:42 +0100

    Create 2lists2listOfTuples.py

---
+++ casting/2lists2listOfTuples.py
@@ -0,0 +1,12 @@
+
+# List1
+Name = ['tom', 'krish', 'nick', 'juli']
+# List2
+Age = [25, 30, 26, 22]
+# get the list of tuples from two lists.
+# and merge them by using zip().
+list_of_tuples = list(zip(Name, Age))
+print("list_of_tuples:", list_of_tuples)
+print(df)
+'''output: 
+list_of_tuples [('tom', 25), ('krish', 30), ('nick', 26), ('juli', 22)]
