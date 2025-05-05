e8520fde870bb828c315c7cc9384f34df4f98bf7
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-05-05 23:43:54 +0100

    Create array2list.py

---
+++ casting/array2list.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# 1d array to list
+create numpy array:
+#arr_1 = np.array([1, 2, 3])
+#use tolist() to convert array to list
+list_1 = arr_1.tolist()
+print(f'List: {list_1}')
+#output: List: [1, 2, 3]
