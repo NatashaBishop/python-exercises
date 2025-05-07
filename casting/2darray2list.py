2965ec15302e395e532459d718d5de6c703cb046
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-05-07 20:29:38 +0100

    Create 2darray2list.py

---
+++ casting/2darray2list.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# create 2d array
+arr_2 = np.array([[1, 2, 3], [4, 5, 6]])
+
+print(f'NumPy Array:\n{arr_2}')
+# the same way as we would do with 1d array:
+list_2 = arr_2.tolist()
+
+print(f'List: {list_2}')
+
+#output is 2d List: [[1, 2, 3], [4, 5, 6]]
