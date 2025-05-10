2bde69d55f743aaf6715b4d045aca36e64ba6a91
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-05-10 22:46:01 +0100

    Update eachElementNewLine.py

--- listManipulation/eachElementNewLine.py
+++ listManipulation/eachElementNewLine.py
@@ -1,6 +1,6 @@
 # declare list:
 list = ['I', 'am', 'xyz']
-#print list in table by calling each element by index
+#print list line by line by calling each element by index
 
 print(list[0])
 print(list[1])
