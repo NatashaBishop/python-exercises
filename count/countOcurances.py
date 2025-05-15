bd0fb0b06bd1000f10e0e6018fcaecf4633d7b14
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-05-15 11:06:08 +0100

    Update countOcurances.py

--- count/countOcurances.py
+++ count/countOcurances.py
@@ -4,3 +4,11 @@ s.count("e")
 
 #or:
 "Green tree".count(e)
+
+#count in sub-string
+#If the second (start) and third (end) arguments are specified, count() only searches within the substring [start:end].
+
+print(s.count('e', 1, 4))
+# output: 2
+
+
