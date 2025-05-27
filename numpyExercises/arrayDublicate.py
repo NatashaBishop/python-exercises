b241f7c86028ce253b97f2ff8aa355705cf619e0
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-05-27 10:42:41 +0100

    Update arrayDublicate.py

--- numpyExercises/arrayDublicate.py
+++ numpyExercises/arrayDublicate.py
@@ -8,3 +8,8 @@ print(x)
 #out:
 #5,6,4
 #5,6,4
+
+# we had assigned x = array1 without using .copy(), modifying x  has altered array1 as well, 
+# because NumPy assigns references by default instead of creating copies.
+# use copy() instead if need to leave array1 attached:
+#x = copy(array1)  # copy() produces a separate array in memory
