5a03d3c5a57dc119d99c7bc70b39a985fd762122
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-05-27 09:43:40 +0100

    Create arrayDublicate.py

---
+++ numpyExercises/arrayDublicate.py
@@ -0,0 +1,7 @@
+import numpy as np
+# Create a Python NumPy array
+array1 = np.array([3,6,4])
+x = array1 # assigned x to array1 (array1 dublicate)
+x[0] = 5 #Modify the first element of x
+print(array1)
+print(x)
