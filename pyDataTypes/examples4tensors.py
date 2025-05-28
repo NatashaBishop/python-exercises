f07cf7be83a5d493ac0505b83efa51fd375370e7
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-05-28 17:53:50 +0100

    Update examples4tensors.py

--- pyDataTypes/examples4tensors.py
+++ pyDataTypes/examples4tensors.py
@@ -25,7 +25,7 @@ c = torch.add(a,b)
 aa = torch.tensor([[1,2,3,4],[2,3,4,5]])
 bb = torch.tensor([[4,3,2,1],[5,4,3,2]])
 
-print(aa.shape) #the structure of aa
+print(aa.shape) #the structure of aa. Shape of 'aa' -> (2, 4)
 #print(help(torch.mm)) #mm 4 matrix multiplication
 print(torch.mm(aa,bb.T)) #T 4 transpose
 
