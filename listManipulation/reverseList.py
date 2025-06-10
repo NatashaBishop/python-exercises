b9de914d4b958dbcea9bb934f30420327b871a34
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-06-10 23:39:01 +0100

    Update reverseList.py

--- listManipulation/reverseList.py
+++ listManipulation/reverseList.py
@@ -1,5 +1,7 @@
 # Write a function to reverse a given list
 
-list = [1, 2, 3, 4, 5, 6]
-     def reverse_list(lst):
-    return lst[::-1]
+list1 = [1, 2, 3, 4, 5, 6]
+def reverse_list(list1):
+    return list1[::-1]
+
+print(reverse_list([list1]))
