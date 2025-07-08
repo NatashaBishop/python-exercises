1909588c3e21f653d7a6836707baea49e74ff508
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-07-09 00:19:43 +0100

    Update pandasConcatWithKeys.py

--- pandas/pandasConcatWithKeys.py
+++ pandas/pandasConcatWithKeys.py
@@ -1,3 +1,5 @@
+
+#Adding hierarchical index at the outermost level of the data with the keys option
 s1 = pd.Series(['a', 'b'])
 s2 = pd.Series(['c', 'd'])
 pd.concat([s1, s2], keys=['s1', 's2']) '''
