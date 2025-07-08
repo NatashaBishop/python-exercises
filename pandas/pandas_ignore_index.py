56f2728d5f4b29ce0316c1f0972fb8e49f7dca9f
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-07-08 23:33:32 +0100

    Update pandas_ignore_index.py

--- pandas/pandas_ignore_index.py
+++ pandas/pandas_ignore_index.py
@@ -1,4 +1,4 @@
-
+# Clear the existing index and reset it in the result by setting the ignore_index option to True.
 s1 = pd.Series(['a', 'b'])
 s2 = pd.Series(['c', 'd'])
 pd.concat([s1, s2], ignore_index=True) '''
