154f88e76107964211c767a4ab333dd6ff23de2b
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-07-11 10:50:17 +0100

    Create pandas_merge.py

---
+++ pandas/pandas_merge.py
@@ -0,0 +1,26 @@
+df1 = pd.DataFrame({'lkey': ['foo', 'bar', 'baz', 'foo'], 'value': [1, 2, 3, 5]})
+df2 = pd.DataFrame({'rkey': ['foo', 'bar', 'baz', 'foo'], 'value': [5, 6, 7, 8]})
+'''output:
+    lkey value
+0   foo      1
+1   bar      2
+2   baz      3
+3   foo      5
+
+df2
+    rkey value
+0   foo      5
+1   bar      6
+2   baz      7
+3   foo      8'''
+
+# Merge df1 and df2 on the lkey and rkey columns. The value columns have the default suffixes, _x and _y, appended.
+df1.merge(df2, left_on='lkey', right_on='rkey')
+'''output:
+  lkey  value_x rkey  value_y
+0  foo        1  foo        5
+1  foo        1  foo        8
+2  bar        2  bar        6
+3  baz        3  baz        7
+4  foo        5  foo        5
+5  foo        5  foo        8'''
