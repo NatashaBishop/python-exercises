903c04dd48871ea7fd6487999d2e7182c78ad154
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-07-12 14:18:13 +0100

    Update pandas_exel2dataframe.py

--- pandas/pandas_exel2dataframe.py
+++ pandas/pandas_exel2dataframe.py
@@ -1,4 +1,8 @@
+#Class for parsing tabular Excel sheets into DataFrame objects: pandas.ExcelFile
+#class pandas.ExcelFile(path_or_buffer, engine=None, storage_options=None, engine_kwargs=None)
 
+
+Class for parsing tabular Excel sheets into DataFrame objects.
 file = pd.ExcelFile('myfile.xlsx')  
 with pd.ExcelFile("myfile.xls") as xls:  
 df1 = pd.read_excel(xls, "Sheet1")  
