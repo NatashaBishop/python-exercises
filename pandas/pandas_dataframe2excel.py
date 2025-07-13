3b29dfa65a4e713ecb1a01384c60036e70ba5f56
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-07-13 01:26:30 +0100

    Update pandas_dataframe2excel.py

--- pandas/pandas_dataframe2excel.py
+++ pandas/pandas_dataframe2excel.py
@@ -1,4 +1,8 @@
+#
+
 df1 = pd.DataFrame([['a', 'b'], ['c', 'd']],
 index=['row 1', 'row 2'],
 columns=['col 1', 'col 2'])
 df1.to_excel("output.xlsx")  
+#specify the sheet name:
+df2.to_excel("output.xlsx", sheet_name='Sheet_name_1')
