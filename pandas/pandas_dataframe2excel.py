76d51cedd239ddc92984f5c0ccb7228e43520959
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-07-14 01:33:20 +0100

    Update pandas_dataframe2excel.py

--- pandas/pandas_dataframe2excel.py
+++ pandas/pandas_dataframe2excel.py
@@ -6,7 +6,9 @@ columns=['col 1', 'col 2'])
 df1.to_excel("output.xlsx")  
 #specify the sheet name:
 df2.to_excel("output.xlsx", sheet_name='Sheet_name_1')
+
 # writing 2 datasets into one excel file
+# write to multiple sheets: we need to create an ExcelWriter object with a target file name, and specify a sheet in the file to write to.
 df2 = df1.copy() #create 2nd dataset
 with pd.ExcelWriter('output.xlsx') as writer:  
     df1.to_excel(writer, sheet_name='Sheet_name_1')
