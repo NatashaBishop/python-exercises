9c67f52eb63aa1c9912645703274977484e03e8c
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-08-25 18:06:27 +0100

    Fix constructor method name in BankAccount class

--- BankAccountManager/bank_account_manager.py
+++ BankAccountManager/bank_account_manager.py
@@ -1,4 +1,4 @@
 class BankAccount:
-    def init(self, owner: str, balance: float = 0.0):
+    def __init__(self, owner: str, balance: float = 0.0):
         self.owner = owner
         self.balance = balance
