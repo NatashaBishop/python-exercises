8609471e5bb3de1d7e27436333215b03a843a09b
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-09-10 10:57:22 +0100

    Update createDictionary.py

--- pyDataTypes/dictionary/createDictionary.py
+++ pyDataTypes/dictionary/createDictionary.py
@@ -2,3 +2,8 @@
 d1 = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
 print(d1)
 #output: {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+
+# can use dict() constructor:
+d2 = dict(a = "Geeks", b = "for", c = "Geeks")
+print(d2)
+#output: {'a': 'Geeks', 'b': 'for', 'c': 'Geeks'}
