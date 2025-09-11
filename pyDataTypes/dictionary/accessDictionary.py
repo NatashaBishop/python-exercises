a235dcab4d91d078b9722ab2800b4d06e91a782a
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-09-11 16:36:22 +0100

    Include get() method for dictionary access
    Added example of accessing dictionary using get() method.

--- pyDataTypes/dictionary/accessDictionary.py
+++ pyDataTypes/dictionary/accessDictionary.py
@@ -1,4 +1,8 @@
 d = { "name": "Prajjwal", 1: "Python", (1, 2): [1,2,4] }
 # access dictionary using dictionary key:
 print(d["name"])
-#Output: Prajjwal
+
+# access dictionary using get()
+print(d.get("name"))
+
+#Output in both cases: Prajjwal
