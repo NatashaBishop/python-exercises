3d94582441232bb228724d44852c3d45bf21f722
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-09-14 17:25:57 +0100

    Create installPIP.py

---
+++ installPIP.py
@@ -0,0 +1,17 @@
+# download and install it from this page: https://pypi.org/project/pip/
+# Download a Package
+'''
+Downloading a package is very easy.
+Open the command line interface and tell PIP to download the package you want.
+Navigate your command line to the location of Python's script directory, and type the following:
+Example: 
+Download a package named "camelcase":
+C:\Users\Your Name\AppData\Local\Programs\Python\Python36-32\Scripts>pip install camelcase
+'''
+# Check PIP version:
+# C:\Users\Your Name\AppData\Local\Programs\Python\Python36-32\Scripts>pip --version 
+# Example: Import and use "camelcase":
+import camelcase
+camel = camelcase.CamelCase()
+text = "hello world"
+print(camel.hump(text)) 
