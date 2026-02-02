c6ed3bb0c5956023d743541923bc3d6619a76114
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2026-02-02 17:04:18 +0000

    accessing elements in list in 2 ways
    Added a comment explaining how to access the first element.

--- accessElements/accessLastIndex.py
+++ accessElements/accessLastIndex.py
@@ -4,3 +4,5 @@ print(motorcycles[0])    # honda
 print(motorcycles[1])    # yamaha
 print(motorcycles[-2])   # yamaha
 print(motorcycles[-3])   # honda
+
+ # honda can b accessed in 2 ways: [-3] - third element from the end and [0] - first element from the start
