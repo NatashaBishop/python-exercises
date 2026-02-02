04c2b97fbb17bf9604d5d2afef709d762c0a9fa3
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2026-02-02 16:57:37 +0000

    Add examples for accessing motorcycle list elements

--- accessElements/accessLastIndex.py
+++ accessElements/accessLastIndex.py
@@ -1,2 +1,6 @@
 motorcycles = ['honda', 'yamaha', 'suzuki'] 
 print(motorcycles[-1]) #access last index
+print(motorcycles[0])    # honda
+print(motorcycles[1])    # yamaha
+print(motorcycles[-2])   # yamaha
+print(motorcycles[-3])   # honda
