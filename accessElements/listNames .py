9137780eb4d4d3c6030cc1655c6c6e546d214dbb
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2026-02-08 22:03:59 +0000

    Update listNames .py

--- accessElements/listNames .py
+++ accessElements/listNames .py
@@ -1,11 +1,10 @@
-names = ["Ana", "Ben", "Cara", "Dan"]
+names = ["Ana", "Ben", "Cara", "Dan", "Alex"]
 
-print(names[1])     # second name
-print(names[-1])    # last name
-print(names[-2])    # second-to-last
+print(names[2])    # 3rd from the start
+print(names[-2])   # 2nd from the end
 #output:
-#Ben
-#Dan
 #Cara
+#Dan
+
 
 # index [2] in 3rd from the start, but index [-2] is 2nd from the end :-)
