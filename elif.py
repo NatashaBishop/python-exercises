fa86cd618ca49f9f013d65beea9d5412af5b447a
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2026-02-21 00:11:49 +0000

    Add grading system based on score

--- elif.py
+++ elif.py
@@ -4,3 +4,15 @@ if b > a:
   print("b is greater than a")
 elif a == b:
   print("a and b are equal")
+
+#multiple conditions
+score = 75
+
+if score >= 90:
+  print("Grade: A")
+elif score >= 80:
+  print("Grade: B")
+elif score >= 70:
+  print("Grade: C")
+elif score >= 60:
+  print("Grade: D")
